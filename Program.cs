7b1e6f68cbbff8d2f895d0cd42602d60607e026f
mayoo <mayoozin.developer@gmail.com>
2024-04-19 01:03:35 +0630

    - [ using ] FastEndpoints.Security

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 global using FastEndpoints;
+global using FastEndpoints.Security;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
@@ -6,11 +7,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+
+//}
 app.MapGet("/", () => "Hello World!");
 app.UseFastEndpoints();
 app.Run();
