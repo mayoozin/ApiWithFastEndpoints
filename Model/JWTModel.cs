24e962b6f34fbe2c4da47a6394e56dc4500da780
mayoo <mayoozin.developer@gmail.com>
2024-04-19 02:30:00 +0630

    - [ Added ] JWT Model

---
+++ Model/JWTModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiWithFastEndpoints.Model
+{
+    public class JWTModel
+    {
+        public string JwtToken { get; set; }
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string TokenExpiry { get; set; }
+
+    }
+}
