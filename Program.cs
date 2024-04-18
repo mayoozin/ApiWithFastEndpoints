13969cce09d8939e094c9127e8d6e9e14a4a4ee7
mayoo <mayoozin.developer@gmail.com>
2024-04-19 01:05:10 +0630

    - [ Add Auth JWT Bearer ]

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@ global using FastEndpoints;
 global using FastEndpoints.Security;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthenticationJwtBearer(s => s.SigningKey = "The secret used to sign tokens") //add this
+   .AddAuthorization();
 builder.Services.AddFastEndpoints();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseAuthentication() //add this
+   .UseAuthorization();
 app.UseSwagger();
 app.UseSwaggerUI();
 //if (app.Environment.IsDevelopment())
