cb8313d35019cc60e711135fd19eee71c0333fa6
mayoo <mayoozin.developer@gmail.com>
2024-04-19 01:43:28 +0630

    -[ Jwt ] Token Modified

--- Program.cs
+++ Program.cs
@@ -11,14 +11,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.UseAuthentication() //add this
    .UseAuthorization();
-app.UseSwagger();
-app.UseSwaggerUI();
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-//}
+}
 app.MapGet("/", () => "Hello World!");
 app.UseFastEndpoints();
 app.Run();
