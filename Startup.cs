769622eb6d043bd9711e8a1e2ec2468815c32f92
mayoo <mayoozin.developer@gmail.com>
2024-04-19 02:29:36 +0630

    - [ Added ] Starup File

---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using ApiWithFastEndpoints.Model;
+using FastEndpoints;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ApiWithFastEndpoints
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<JWTModel>(Configuration.GetSection("JWTModel"));
+        }
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment)
+        //{
+        //  var user=  Configuration.GetSection("HangfireSettings:UserName").Value,
+        // Configuration.GetSection("HangfireSettings:Password").Value
+        //}
+    }
+}
